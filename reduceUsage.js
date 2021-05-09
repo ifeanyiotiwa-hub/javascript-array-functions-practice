8873c0aaee33c6b1016d481bc91197a6493c33a4
ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-05-09 19:12:22 +0100

    Started Reduce() exercise in reduceUsage.js file

---
+++ reduceUsage.js
@@ -0,0 +1,56 @@
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//***REDUCE***
+//1. Get total mass of all characters
+
+    let totalMass = characters.reduce((accumulator,currentValue) => {
+        return accumulator + currentValue.mass;
+    },0);
+
+    console.log(totalMass)
+//2. Get total height of all characters
+    let totalHeight = characters.reduce((accumulator,currentValue) => {
+        return accumulator + currentValue.height;
+    }, 0);
+    console.log(totalHeight);
+//3. Get total number of characters by eye color
+    const totalByEyeColor  = characters.reduce( (accumulator, currentValue) => {
+        if (!accumulator['eye_color']){
+            accumulator['eye_color'] = currentValue.eye_color;
+        } else{
+            accumulator['eye_color'] += currentValue.eye_color;
+        }
+        return accumulator;
+    },{})
+
+    console.log(totalByEyeColor);
+//4. Get total number of characters in all the character names
