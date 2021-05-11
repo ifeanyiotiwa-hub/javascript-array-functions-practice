cdd867dbef3f719cc23f9b8bec682a98b1f851d9
ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-05-11 15:48:59 +0100

    This commit added arrayalgo.js containing a function that returns the number of true values in an array

---
+++ arrayalgo.js
@@ -0,0 +1,35 @@
+// Create a function which returns the number of true values there are in an array.
+//EXAMPLES
+//countTrue([true, false, false, true, false]) ➞ 2
+//countTrue([false, false, false, false]) ➞ 0
+//countTrue([]) ➞ 0
+
+/*
+Notes
+1. Return 0 if given an empty array.
+2. All array items are of the type bool (true or false).
+ */
+
+function countTrue(arr) {
+	let store = {};
+	arr.map(item => {
+		if(!store[item]){
+			store[item] = 1
+		}else{
+			store[item]++
+		}
+	})
+	
+	if (!store['true']){
+		return 0
+	}
+	
+    return store['true'];
+	 
+}
+
+
+console.log(countTrue([true, false, false, true, false]));
+console.log(countTrue([false, false, false, false]));
+console.log(countTrue([]));
+console.log(countTrue([false, false, true, true, false, false, false, true, true, true, true, false, true, true, false]))
