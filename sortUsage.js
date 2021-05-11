d6cf643acd231e33b88c22af23189b8e7834355c
ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-05-11 16:23:17 +0100

    Creates sortUsage.js to solve the chalenges. This contains only solution for 1 and 2

---
+++ sortUsage.js
@@ -0,0 +1,44 @@
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+
+//***SORT***
+//1. Sort by mass
+const mass = (a, b) => a.mass - b.mass;
+const sortByMass = characters.sort(mass);
+//console.log(sortByMass);
+//2. Sort by height
+const height = (a, b) => a.height - b.height;
+const sortByHeight = characters.sort(height);
+console.log(sortByHeight);
+
+//3. Sort by name
+//4. Sort by gender
