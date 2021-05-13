132be8452f244ae0da1a5b42f01f612e6a75a099
ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-05-13 22:34:00 +0100

    This commit completes the sort challenge

--- sortUsage.js
+++ sortUsage.js
@@ -34,11 +34,21 @@ const characters = [
 //1. Sort by mass
 const mass = (a, b) => a.mass - b.mass;
 const sortByMass = characters.sort(mass);
-//console.log(sortByMass);
+console.log(sortByMass);
 //2. Sort by height
 const height = (a, b) => a.height - b.height;
 const sortByHeight = characters.sort(height);
 console.log(sortByHeight);
 
 //3. Sort by name
+const nameSort = characters.sort((a, b) => {
+if(a.name > b.name) return 1;
+return -1;
+})
+console.log(nameSort)
 //4. Sort by gender
+const sortByGender = characters.sort(a => {
+    if(a.gender=== "female") return -1;
+    return 1;
+})
+console.log(sortByGender);
